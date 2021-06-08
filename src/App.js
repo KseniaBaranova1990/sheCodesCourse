f7cca6b2f548b11049e855304cdd4cfbc7f435d2
Ksenia Baranova <baranovaksenia1990@gmail.com>
2021-06-08 21:21:45 +0300

    cards-color

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from "react";
-import "./App.css";
-import School from "./School.js";
-
-const studentsGrades = [
-  { name: "Naama", grade: "90", id: 0 },
-  { name: "David", grade: "80", id: 1 },
-  { name: "Lior", grade: "95", id: 2 },
-  { name: "Yaniv", grade: "100", id: 3 },
-];
+import Card from "./components/Card";
 
 function App() {
-  return <School school={studentsGrades} />;
+  return (
+    <div>
+      <Card color="#FFA737" />
+      <Card color="#FF6663" />
+    </div>
+  );
 }
 
 export default App;
